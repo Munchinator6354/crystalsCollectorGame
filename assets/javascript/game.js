8aa1260f0e1e37c07a9565cebff0e2c95eba0444
Ryan Isaacson <munchinator6354@gmail.com>
2019-10-11 20:56:54 -0700

    added buttons rather than spans for gems, then worked with tutor to update the CSS to center stubborn gems, then helped format the game to make buttons actually do stuff when clicked, game currently meets MVP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,80 @@
 
 // Starting global variables
-
 // randomTargetNumber variable must be between 19-120
-var randomTargetNumber = Math.floor(Math.random() * 121);
+// Generate random number for each crystal from 1-12
+var min = 19;
+var max = 120;
+var randomTargetNumber ;
 var startingScore = 0;
 var wins = 0;
 var losses = 0;
+var randomEmeraldNumber ;
+var randomRubyNumber ;
+var randomSapphireNumber ;
+var randomTopazNumber ;
 
+function startGame() {
+    randomTargetNumber = getRandom(19,120);
+    startingScore = 0;
+    randomEmeraldNumber = getRandom(1,12);
+    randomRubyNumber = getRandom(1,12);
+    randomSapphireNumber = getRandom(1,12);
+    randomTopazNumber = getRandom(1,12);
+    $("#randomTargetNumber").text("Random Start Number: " + randomTargetNumber);
+    $("#currentScore").text("Current Score: " + startingScore);
+}
 
-// Select HTML elements by ID and add the associated starting variables
-$("#randomTargetNumber").append(randomTargetNumber);
-$("#currentScore").append(startingScore);
-$("#wins").append(wins);
-$("#losses").append(losses);
+function getRandom(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function addValue(crystal) {
+    if (crystal === "emerald") {
+        startingScore += randomEmeraldNumber;
+    }
+    else if (crystal === "ruby") {
+        startingScore += randomRubyNumber;
+    }
+    else if (crystal === "sapphire") {
+        startingScore += randomSapphireNumber;
+    }
+    else if (crystal === "topaz") {
+        startingScore += randomTopazNumber;
+    }
+    $("#currentScore").text("Current Score: " + startingScore);
+    checkIfWon();
+}
 
-// Generate random number for each crystal from 1-12
-var randomEmeraldNumber = Math.floor(Math.random() * 13);
-var randomRubyNumber = Math.floor(Math.random() * 13);
-var randomSapphireNumber = Math.floor(Math.random() * 13);
-var randomTopazNumber = Math.floor(Math.random() * 13);
+function checkIfWon() {
+    if (startingScore > randomTargetNumber) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        alert("Sorry, You Lost : (");
+        startGame();
+    }
 
+    else if (startingScore === randomTargetNumber) {
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        alert("You Won!");
+        startGame();
+    } 
+}
 
+startGame();
 
+$("#emeraldButton").click(function () {
+    addValue("emerald");
+});
 
+$("#rubyButton").click(function () {
+    addValue("ruby");
+});
 
+$("#sapphireButton").click(function () {
+    addValue("sapphire");
+});
 
-
-
-
+$("#topazButton").click(function () {
+    addValue("topaz");
+});
